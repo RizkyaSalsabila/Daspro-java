7278ef00fb0966b1d524b8ae36e6183e8bcad542
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-10-06 08:34:21 +0700

    first commit

---
+++ jobsheet_1/Latihan24.java
@@ -0,0 +1,18 @@
+/**
+ * Latihan24
+ */
+public class Latihan24 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Rizkya Salsabila");
+        System.out.print("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama\t : Rizkya Salsabila");
+        System.out.println("Absen\t : 24");
+        System.out.println("Kelas\t : 1D TI");
+        System.out.println("Prodi\t : D4 Teknik Informatika");
+        System.out.println("Jurusan\t : Teknologi Informasi");
+
+    }
+}
---
+++ jobsheet_1/MyFirstJava24.java
@@ -0,0 +1,7 @@
+public class MyFirstJava24 {
+    public static void main (String [] args) {
+
+        System.out.println("Nama saya Rizya Salsabila"); //Output Nama saya Rizya Salsabila
+
+    }
+}
