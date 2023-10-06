fa6b24a1ffe49486217b4048d001d20db144f50a
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-10-06 17:25:13 +0700

    System Login

---
+++ jobsheet_5/LatihanIndividu_24/Login24.java
@@ -0,0 +1,31 @@
+package LatihanIndividu_24;
+
+import java.util.Scanner;
+public class Login24 {
+    public static void main (String[] args) {
+        //Deklarasi Scanner
+        Scanner username24 = new Scanner(System.in);
+
+        //Buat variabel inputUser, inputPassword
+        String inputUser, inputPassword;
+
+        System.out.println("--- LOGIN ---");
+        System.out.println("Selamat Datang di Aplikasi Sederhana");
+
+        //Masukkan username
+        System.out.print("\nMasukkan Username : ");
+        inputUser = username24.nextLine();
+
+        //Masukkan password
+        System.out.print("Masukkan Password : ");
+        inputPassword = username24.nextLine();
+
+        if (inputUser.equals("Pengguna") && inputPassword.equals("123")) {
+            System.out.println("Login Berhasil");
+        } else {
+            System.out.println("Username atau Password Anda salah! Silahkan coba lagi");
+        }
+
+        username24.close();
+    }
+}
