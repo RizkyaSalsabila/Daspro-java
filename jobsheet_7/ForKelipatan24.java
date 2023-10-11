105831b64314d02c20c466028fcaffc570074798
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-10-11 12:50:41 +0700

    Menambah variabel rata-rata

--- jobsheet_7/ForKelipatan24.java
+++ jobsheet_7/ForKelipatan24.java
@@ -8,7 +8,7 @@ public class ForKelipatan24 {
 
         //Buat variabel 
         int kelipatan, total = 0, counter = 0;
-        float rataRata = 0;     //menambahkan variabel baru
+        double rataRata = 0;     //menambahkan variabel baru
 
         //Input bilangan kelipatan
         System.out.print("Masukkan bilangan kelipatan (1 - 9) : ");
@@ -19,13 +19,14 @@ public class ForKelipatan24 {
             if (i % kelipatan == 0) {
                 total += i;
                 counter++;
-                rataRata = (float) total / counter;     //Rumus rata-rata
             }
         }
+        
+        rataRata = (double) total / counter;     
         
         //Output banyak dan total bilangan kelipatan yang dimasukkan
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
-        System.out.printf("Rata - rata banyaknya kelipatan bilangan %d dari 1 sampai 50 adalah %f\n", kelipatan, rataRata);     //Output rata-rata
+        System.out.printf("Rata - rata banyaknya kelipatan bilangan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);     //Output rata-rata
     }
 }
