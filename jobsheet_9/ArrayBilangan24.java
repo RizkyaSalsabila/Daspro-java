4c66bde35ab852e7be16eab704cbe1fe3c5afd26
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-10-24 18:32:00 +0700

    Penerapan bilangan menggunakan array

---
+++ jobsheet_9/ArrayBilangan24.java
@@ -0,0 +1,20 @@
+public class ArrayBilangan24 {
+    public static void main(String[] args) {
+        
+        //Deklarasikan array bertipe int
+        int[] bil = new int[4];
+
+        //Isi setiap elemen pada array
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        //Cetak array
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+    }
+}
