451c8332508ea0df770c9cfacd7014b679ce0039
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-10-31 19:10:27 +0700

    Menambahkan fitur searching pada key

--- jobsheet_9/Searching24.java
+++ jobsheet_9/Searching24.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Searching24 {
     public static void main(String[] args) {
         
-        //Inisialisasi array berupa integer
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        //Buat variabel key, hasil
-        int key = 20;
+        //Deklarasi Scanner
+        Scanner input24 = new Scanner(System.in);
+
+        //input jumlah elemen array
+        System.out.print("Masukkan jumlah elemen array : ");
+        int n = input24.nextInt();
+        
+        //Deklarasi array berupa integer
+        int[] arrayInt = new int[n];
+        //Buat variabel hasil
         int hasil = 0;
+
+        //Array loop untuk input elemen array
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke - " + i + " : ");
+            arrayInt[i] = input24.nextInt();
+        }
+
+        //input key yang ingin dicari
+        System.out.print("Masukkan key yang ingin dicari : ");
+        int key = input24.nextInt();
 
         //Array Loop sesuai panjang arrayInt
         for (int i = 0; i < arrayInt.length; i++) {
@@ -14,8 +32,9 @@ public class Searching24 {
                 break;
             }
         }
-
         //Cetak posisi key dalam array
-        System.out.println("Key ada dalam array pada posisi indeks ke- " + hasil);
+        System.out.println("Key ada di posisi indeks ke - " + hasil);
+
+        input24.close();
     }
 }
