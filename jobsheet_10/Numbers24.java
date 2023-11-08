4ea903a57dc9a87592c1032dfa42ecd962920cb3
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-08 08:14:49 +0700

    Menampilkan isi semua elemen

--- jobsheet_10/Numbers24.java
+++ jobsheet_10/Numbers24.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers24 {
     public static void main(String[] args) {
         //Array 2 dimensi dengan length berbeda tiap baris
@@ -5,5 +7,9 @@ public class Numbers24 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }   
     }
 }
