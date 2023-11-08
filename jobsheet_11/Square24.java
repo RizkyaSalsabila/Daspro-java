9392b19622d65b26317db59a44df33882f0d2129
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-08 13:56:54 +0700

    Penerapan Square

---
+++ jobsheet_11/Square24.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Square24 {
+    public static void main (String[] args) {
+        //Deklarasi Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Masukkan N
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        //Cetak * sebanyak N kali
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
