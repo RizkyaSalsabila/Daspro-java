cca198e3ad7f587a1a51b5af770f98249b753dc3
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-08 07:23:39 +0700

    Penerapan array 2 dimensi dengan length berbeda tiap baris

---
+++ jobsheet_10/Numbers24.java
@@ -0,0 +1,9 @@
+public class Numbers24 {
+    public static void main(String[] args) {
+        //Array 2 dimensi dengan length berbeda tiap baris
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+    }
+}
