5db5e2949ffe5377c8fc1997b98be478b4415fcf
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-22 12:29:01 +0700

    Penerapan fungsi tanpa parameter

---
+++ jobsheet_13/TerimaKasih_24.java
@@ -0,0 +1,11 @@
+public class TerimaKasih_24 {
+    //Buat fungsi UcapanTerimaKasih
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    //Buat fungsi main
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
