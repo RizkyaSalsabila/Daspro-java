1efd52e53848091218a79740d99e8017eb92d1dd
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-22 14:39:46 +0700

    Penerapan fungsi dengan parameter

--- jobsheet_13/TerimaKasih_24.java
+++ jobsheet_13/TerimaKasih_24.java
@@ -1,7 +1,17 @@
 public class TerimaKasih_24 {
+    //Buat fungsi UcapanTerimaKasih
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    //Buat fungsi dengan parameter bertipe String, namaParameter ucapan
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
 
     //Buat fungsi main
     public static void main(String[] args) {
-        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
