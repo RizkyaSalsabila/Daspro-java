b5834877826562b2db8297de9d6019644ac8f500
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-23 23:05:29 +0700

    Modifikasi calling fungsi

--- jobsheet_13/UcapanTerimaKasih_24.java
+++ jobsheet_13/UcapanTerimaKasih_24.java
@@ -17,8 +17,14 @@ public class UcapanTerimaKasih_24 {
         System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
+    //Buat fungsi UcapanTambahan()
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     //Fungsi main
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        UcapanTambahan("Thank you very much.");
     }
 }
