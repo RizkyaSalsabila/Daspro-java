7fe762dda7f839163fc666c94ee2bf429b79ddd5
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-27 21:32:37 +0700

    Fungsi tambahan mahasiswa nilai tertinggi

--- jobsheet_13/tugas_individu/NilaiMhs24.java
+++ jobsheet_13/tugas_individu/NilaiMhs24.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class NilaiMhs24 {
     //Deklarasi dan instansiasi array 2 dimensi data
-    static int data[][] = new int[2][3];
+    static int data[][] = new int[5][7];
     //Deklarasi dan instansiasi array 1 dimensi nama
-    static String nama[] = new String[2];
+    static String nama[] = new String[5];
 
     //Fungsi inputDataMahasiswa
     static void inputDataMhs() {
@@ -80,11 +80,11 @@ public class NilaiMhs24 {
         
         //Perulangan For Bersarang
         for (int i = 0; i < data.length; i++) {
-            mhs = nama[i];
             for (int j = 0; j < data[i].length; j++) {
                 if (data[i][j] > nilaiTinggi) {
                     nilaiTinggi = data[i][j];
                     mingguKe = j + 1;
+                    mhs = nama[i];
                 }
             }
         }
