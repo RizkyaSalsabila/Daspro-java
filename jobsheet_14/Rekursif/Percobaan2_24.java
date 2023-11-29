b46a8ffe1f12c88c9d7b535554e9beabfd151251
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-29 13:55:19 +0700

    Hitung pangkat

---
+++ jobsheet_14/Rekursif/Percobaan2_24.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Percobaan2_24 {
+    //Buat fungsi hitungPangkat()
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x * hitungPangkat(x, y - 1));
+        }
+    }
+
+    //Buat fungsi method main()
+    public static void main(String[] args) {
+        //Deklarasi Scanner
+        Scanner sc = new Scanner(System.in);
+        //Buat 2 variabel bertipe int
+        int bilangan, pangkat;
+
+        //Input bilangan
+        System.out.print("Bilangan yang dihitung : ");
+        bilangan = sc.nextInt();
+        //Input pangkat
+        System.out.print("Pangkat : ");
+        pangkat = sc.nextInt();
+
+        //Cetak hasil
+        System.out.println(hitungPangkat(bilangan, pangkat));
+    }
+}
